6152ea5a6a6f1f224711944ec8d5cabe7aabb40e
fionaxc <fcai2@stuy.edu>
2018-03-13 08:27:51 -0400

    wrote tester

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -3,10 +3,31 @@
 *
 *
 */
+import java.util.Random;
+
 public class QuickSortTester{
 
+//-------RANDOM ARRAY GENERATOR HELPER---------//
+  public static int[] randomArr(int n){
+    int[] ans = new int[n];
+    for(int i = 0; i<n;i++){
+      ans[i] = Random().nextInt(n);
+    }
+    return ans;
+  }
+
+//--------------MAIN METHOD--------------------//
   public static void main(String[] args) {
     double totalTime = 0;
-    for(int i = 1000)
+    for(int i = 100; i <= 1000; i+=50 ){
+      int[] testArr = randomArr(i);
+      double beginTime = System.nanoTime();
+      QuickSort.qsort(testArr);
+      double endTime = System.nanoTime();
+      totalTime = endTime-beginTime;
+      System.out.println(i + " dim, " + totalTime);
+      totalTime = 0;
+    }
+
   }
 }
